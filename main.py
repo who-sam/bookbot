45544e4c2982ee3a8f18c42d52042eb71108cd0c
who-sam <hossam.samy.rashed@gmail.com>
2025-05-10 03:10:25 +0300

    added counting words function

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("greetings boots")
+def get_book_text(path):
+    file_contents = ""
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def count_words(file):
+    words = file.split()
+    return len(words)
+
+
+
+def main():
+    file_content = get_book_text("./books/frankenstein.txt")
+    num_words = count_words(file_content)
+    print(f"{num_words} words found in the document")
+
+
+
+
+main()
